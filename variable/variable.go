9198c9d562508523d0342d9826c781f615a3694e
Singgih Pratama <singgih.vendor.pasti@pegadaian.co.id>
2023-09-22 09:05:30 +0700

    multipleVariable dan predifinedVariable

--- variable/variable.go
+++ variable/variable.go
@@ -8,8 +8,29 @@ import (
 func main() {
 	var firstName string = "Singgih"
 	var lastName = "Pratama"
-	var age = 25
+	age := 25
 	log.Println(firstName, lastName, age)
 
 	fmt.Printf("Hallo %s %s %v! \n", firstName, lastName, age)
+
+	/*
+		Call func multiVariable
+	*/
+	multiVariable()
+	predifinedVariable()
+}
+
+func multiVariable() {
+	var firstName, middleName, lastName, age = "Muhammad", "Pratama", "Singgih", 20
+
+	one, isFriday, ninePointSix, say := 1, true, 9.6, "wtf"
+
+	log.Printf("Hello %s %s %s %v", firstName, middleName, lastName, age)
+	fmt.Println(one, isFriday, ninePointSix, say)
+}
+
+func predifinedVariable() {
+	var firstName, _ = "Farah", "kuyy"
+
+	log.Printf("Hello %s", firstName)
 }
